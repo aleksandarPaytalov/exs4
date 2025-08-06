3e198c8c3d55468ee253d30d646de924c3ae3211
aleksandarPaytalov <apaytalov89@gmail.com>
2025-08-06 12:11:37 +0300

    storage container added for Tfstate file

--- main.tf
+++ main.tf
@@ -5,6 +5,13 @@ terraform {
       version = "=4.0.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "StorageRG"
+    storage_account_name = "alextaskboardstorage"
+    container_name       = "alextaskboardcontainer"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
